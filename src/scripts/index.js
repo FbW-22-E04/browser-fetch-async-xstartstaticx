647545de7c9fd9f04a15bdfc494ed2f784e17d27
xstartstaticx <daniel.reiller@icloud.com>
2022-09-29 15:41:21 +0200

    finished exercise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,38 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+// let promiseOfModal = new Promise(function (resolve) {
+//   window.setTimeout(function () {
+//     resolve(modal);
+//   }, 1000 * 60);
+// });
+function resolveAfter60Seconds() {
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      resolve(modal);
+    }, 1000 * 60);
+  });
+}
+
+const promiseOfModal = async () => {
+  const result = await resolveAfter60Seconds();
+  console.log("User has been on the page for 60 seconds", result);
+  modal.style.display = "block";
+};
+promiseOfModal();
+
+// promiseOfModal.then(function (val) {
+//   console.log("User has been on the page for 60 seconds");
+//   val.style.display = "block";
+// });
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
-
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
